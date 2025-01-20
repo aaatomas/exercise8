94ee5d0d609f0b422aced45e692cdde8eb501349
Tomas Achramenko <97428356+aaatomas@users.noreply.github.com>
2025-01-20 22:18:53 +0200

    Update tests.py

--- exercise8/app_hello/tests.py
+++ exercise8/app_hello/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class IndexViewTest(TestCase):
+    def test_index_view_status_code(self):
+        response = self.client.get(reverse('app_hello:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_view_contains_correct_text(self):
+        response = self.client.get(reverse('app_hello:index'))
+        self.assertContains(response, "Hello, world. You're at the 'app_hello' index...")
+
+    def test_index_view_returns_correct_template(self):
+        response = self.client.get(reverse('app_hello:index'))
+        self.assertTemplateUsed(response, 'app_hello/index.html')
