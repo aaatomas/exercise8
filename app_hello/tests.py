e647ae27570e48c20ce7a4cba126a8de25acdc62
Tomas <tomas@IT000001s-MacBook-Pro.local>
2025-01-20 22:43:31 +0200

    asdasd

--- app_hello/tests.py
+++ app_hello/tests.py
@@ -5,11 +5,3 @@ class IndexViewTest(TestCase):
     def test_index_view_status_code(self):
         response = self.client.get(reverse('app_hello:index'))
         self.assertEqual(response.status_code, 200)
-
-    def test_index_view_contains_correct_text(self):
-        response = self.client.get(reverse('app_hello:index'))
-        self.assertContains(response, "Hello, world. You're at the 'app_hello' index...")
-
-    def test_index_view_returns_correct_template(self):
-        response = self.client.get(reverse('app_hello:index'))
-        self.assertTemplateUsed(response, 'app_hello/index.html')
